f720edfba0070fae3f7359397d08667cdaeb820d
ravilushqa <ravil@sknt.ru>
2018-04-10 01:39:50 +0300

    add more test cases

--- sicp/chapter1/03.rkt
+++ sicp/chapter1/03.rkt
@@ -13,6 +13,8 @@
 (define (sum-of-squares-two-bigest-numbers x y z)
   (cond((and(>= y x) (>= z x)) (sum-of-squares y z))
        ((and(>= x y) (>= z y)) (sum-of-squares x z))
-       ((and(>= x z) (>= y z)) (sum-of-squares x y))))
+       (else (sum-of-squares x y))))
 
+(check-equal? (sum-of-squares-two-bigest-numbers 2 2 2) 8)
 (check-equal? (sum-of-squares-two-bigest-numbers 5 2 2) 29)
+(check-equal? (sum-of-squares-two-bigest-numbers 1 2 5) 29)
